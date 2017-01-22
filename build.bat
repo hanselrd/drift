bc69bf3bce4aa34b1ce6ea572af3ac356b12b497
hanselrd <hansel809@hotmail.com>
2017-01-22 02:25:01 -0500

    Added build script

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+cd client &&^
+ng build --base-href ./ --prod &&^
+cd .. &&^
+rmdir /S /Q electron\dist &&^
+move /Y client\dist electron\dist &&^
+cd client &&^
+ng build --prod &&^
+cd ..
